fa11684fc2210c1ba863672c55464eed4e32d78b
LuckXP <js.sindler@gmail.com>
2016-05-20 14:16:55 -0600

    delet blog routes, user routes, passport and related config

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var passport =require('passport');
-var mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost/blogpost');
 var router = express.Router();
 var blogPostRouter = require('./routes/blogposts');
-var tweetRoutes = require('./routes/tweet');
 var BlogPost = require('./models/blogpost');
 var flash = require('connect-flash');
 var session = require('express-session');
@@ -36,31 +32,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(require('webpack-hot-middleware')(compiler));
 };
 
-app.use(session({
- secret: 'ilovescotchscotchyscotchscotch'
-})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(session({
- cookie: {
-   maxAge: 60000
- }
-}));
 app.use(flash());
 
-require('./config/passport')(passport);
 
 
 // routes ======================================================================
 
-require('./routes/user.js')(app, passport);
-
-app.use('/api', blogPostRouter);
-app.use('/api/tweets', tweetRoutes);
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 var port = process.env.PORT || 8080;
 
